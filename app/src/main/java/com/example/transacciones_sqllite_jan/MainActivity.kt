ac8d6dd52a8a2552006f1945dec096a90856ec0f
JanCarlosPM <jan.prado133873@est.uca.edu.ni>
2023-05-06 22:04:59 -0600

    Prueba 2

--- app/src/main/java/com/example/transacciones_sqllite_jan/MainActivity.kt
+++ app/src/main/java/com/example/transacciones_sqllite_jan/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 limpiarCampos()
             } catch (ex: Exception) {
                 Toast.makeText(
-                    this@MainActivity, "Error : ${ex.toString()}",
+                    this@MainActivity, " : ${ex.toString()}",
                     Toast.LENGTH_LONG
                 ).show()
             }
